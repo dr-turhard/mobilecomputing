446d81fe1ca08a143f3e6be511e66cbaffb26538
tuukkarauhala <rauhalatuukka@gmail.com>
2025-01-28 14:23:17 +0200

    HW2 In-progress

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import android.content.res.Configuration
 import android.os.Bundle
+import android.provider.Telephony.Sms.Conversations
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +17,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -25,16 +27,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,12 +53,63 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Conversation(SampleData.conversationSample)
+                Main()
                 }
             }
         }
     }
 
+@Composable
+fun Main(){
+    val navController = rememberNavController()
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "menu",
+            modifier = Modifier.padding(innerPadding)
+        ){
+            composable("menu"){ MenuView(navController) }
+            composable("conversation"){ ConversationView(navController) }
+        }
+    }
+}
+
+@Composable
+fun MenuView(navController: NavHostController){
+    Surface(modifier = Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        ) {
+            Text(text = "Conversation view", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { navController.navigate("conversation") }) {
+                Text("Go view a fun conversation!")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            //This displays the conversation view from Homework 1
+            Conversation(SampleData.conversationSample)
+        }
+    }
+}
+
+@Composable
+fun ConversationView(navController: NavHostController){
+    Surface(modifier = Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        ){
+            Text(text = "Menu view", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { navController.popBackStack() }) {
+                Text("Return to previous view")
+            }
+        }
+    }
+}
 
 data class Message(val author: String, val body: String)
 
@@ -100,6 +162,9 @@ fun MessageCard(msg: Message){
         }
     }
 }
+
+
+
 @Composable
 fun Conversation(messages: List<Message>){
     LazyColumn {
@@ -131,6 +196,8 @@ fun PreviewMessageCard(){
             )
         }
     }
+
+
 
 
 }
